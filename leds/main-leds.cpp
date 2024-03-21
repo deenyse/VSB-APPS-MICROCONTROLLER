e69bb7076f367303012f1f351b850c3384bbbb8b
deenyse <47937619+deenyse@users.noreply.github.com>
2024-03-21 17:12:52 +0100

    Leds task

--- leds/main-leds.cpp
+++ leds/main-leds.cpp
@@ -1,89 +1,61 @@
-// **************************************************************************
-//
-//               Demo program for APPS labs
-//
-// Subject:      Computer Architectures and Parallel systems
-// Author:       Petr Olivka, petr.olivka@vsb.cz, 03/2024
-// Organization: Department of Computer Science, FEECS,
-//               VSB-Technical University of Ostrava, CZ
-//
-// File:         Main program for LEDs
-//
-// **************************************************************************
-
 #include "mbed.h"
 
-// LEDs on K64F-KIT - instances of class DigitalOut
-DigitalOut g_led_PTA1( PTA1 );
-DigitalOut g_led_PTA2( PTA2 );
+class ledcontrols {
+private:
+    Ticker m_ticker;
+    Ticker b_ticker;
+    DigitalOut m_led;
+    bool isIncrease= true;
+    float max_delay_period = 23;
+    float current_delay = 10;
+    int current_tick = 0;
 
-// Button on K64F-KIT - instance of class DigitalIn
-DigitalIn g_but_PTC9( PTC9 );
-DigitalIn g_but_PTC10( PTC10 );
-DigitalIn g_but_PTC11( PTC11 );
-DigitalIn g_but_PTC12( PTC12 );
+public:
+    ledcontrols(PinName led) : m_led(led) {
+    	this->m_ticker.attach(callback(this, &ledcontrols::pwm), 1ms);
+    	this->b_ticker.attach(callback(this, &ledcontrols::ticker_brightness),5ms);
+    }
 
-void control_from_ticker()
-{
-	static int l_ticks = 0;
+    void ticker_brightness(){
+    	if (this->isIncrease){
+    		this->current_delay += this->max_delay_period*0.001;
+    		if (this->current_delay >= this->max_delay_period){
+    			this->isIncrease = false;
+    		}
 
-	int32_t l_periode = 500;		// T = 0.5 sec
+    	} else {
+    		this->current_delay -= this->max_delay_period*0.001;
+    		if (this->current_delay <=0){
+    			this->isIncrease = true;
+    		}
+    	}
 
-	if ( g_but_PTC9 == 0 )			// check button
-	{
-		l_periode /= 10;			// increase speed of blinking
-	}
+    }
+    void set_brightness(float brightness) {
+    	if (brightness >1)
+    		brightness = 1;
+		if (brightness <0)
+			brightness = 0;
+        this->current_delay = (max_delay_period * (1 - brightness));
+    }
 
-	if ( l_ticks < l_periode / 2 )	// time to switch on or off?
-	{
-		g_led_PTA1 = 0;				// LED Off
-	}
-	else
-	{
-		g_led_PTA1 = 1;				// LED On
-	}
+    void pwm() {
+        if (this->current_tick < this->current_delay)
+            this->m_led.write(0);
+        else
+            this->m_led.write(1);
 
-	l_ticks++;						// milliseconds counter
-	if ( l_ticks >= l_periode )		// end of periode?
-	{
-		l_ticks = 0;				// start periode again
-	}
-}
+        this->current_tick++;
+        if (this->current_tick >= this->max_delay_period) {
+            this->current_tick = 0;
+        }
+    }
 
-int main()
-{
-	printf( "LED demo program started...\n" );
+};
 
-#if 1
-	// modern approach with timer (and interrupt)
-	Ticker l_ticker;
+ledcontrols g_red_led[] = { { PTC1 }, { PTC0 }, { PTC7 }, { PTC3 }, { PTC4 }, { PTC5 }, { PTC2 }, { PTC8 } , {PTA1} , {PTA2} };
 
-	l_ticker.attach( control_from_ticker, 1ms );
 
-	while ( 1 )
-	{ // infinite loop
-	}
-
-#else
-
-	// lazy (conservative) approach
-	while ( 1 )
-	{
-		int32_t l_periode = 500000;	// T = 500000 us = 0.5 sec
-
-		if (g_but_PTC9 == 0) 		// button pressed?
-		{
-			l_periode /= 10;	    // speed up blinking
-		}
-
-		g_led_PTA1 = 0;				// LED off
-
-		wait_us( l_periode / 2 );
-
-		g_led_PTA1 = 1;				// LED on
-
-		wait_us( l_periode / 2 );
-	}
-
-#endif
+int main() {
+    while (1) {}
 }
