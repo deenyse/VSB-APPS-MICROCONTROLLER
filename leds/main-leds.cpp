edac31f4a5004197794176117849cc7e9c3190cc
deenyse <47937619+deenyse@users.noreply.github.com>
2024-03-28 10:29:49 +0100

    added button state

--- leds/main-leds.cpp
+++ leds/main-leds.cpp
@@ -2,60 +2,88 @@
 
 class ledcontrols {
 private:
-    Ticker m_ticker;
-    Ticker b_ticker;
-    DigitalOut m_led;
-    bool isIncrease= true;
-    float max_delay_period = 23;
-    float current_delay = 10;
-    int current_tick = 0;
+	Ticker m_ticker;
+	Ticker b_ticker;
+	DigitalOut m_led;
+	float max_delay_period = 25;
+	float current_delay = 10;
+	int current_tick = 0;
 
 public:
-    ledcontrols(PinName led) : m_led(led) {
-    	this->m_ticker.attach(callback(this, &ledcontrols::pwm), 1ms);
-    	this->b_ticker.attach(callback(this, &ledcontrols::ticker_brightness),5ms);
-    }
+	ledcontrols(PinName led) :
+			m_led(led) {
+		this->m_ticker.attach(callback(this, &ledcontrols::pwm), 1ms);
+	}
 
-    void ticker_brightness(){
-    	if (this->isIncrease){
-    		this->current_delay += this->max_delay_period*0.001;
-    		if (this->current_delay >= this->max_delay_period){
-    			this->isIncrease = false;
-    		}
-
-    	} else {
-    		this->current_delay -= this->max_delay_period*0.001;
-    		if (this->current_delay <=0){
-    			this->isIncrease = true;
-    		}
-    	}
-
-    }
-    void set_brightness(float brightness) {
-    	if (brightness >1)
-    		brightness = 1;
-		if (brightness <0)
+	void set_brightness(float brightness) {
+		if (brightness > 1)
+			brightness = 1;
+		if (brightness < 0)
 			brightness = 0;
-        this->current_delay = (max_delay_period * (1 - brightness));
-    }
+		this->current_delay = (max_delay_period * (1 - brightness));
+	}
 
-    void pwm() {
-        if (this->current_tick < this->current_delay)
-            this->m_led.write(0);
-        else
-            this->m_led.write(1);
+	void pwm() {
+		if (this->current_tick < this->current_delay)
+			this->m_led.write(0);
+		else
+			this->m_led.write(1);
 
-        this->current_tick++;
-        if (this->current_tick >= this->max_delay_period) {
-            this->current_tick = 0;
-        }
-    }
+		this->current_tick++;
+		if (this->current_tick >= this->max_delay_period) {
+			this->current_tick = 0;
+		}
+	}
 
 };
 
-ledcontrols g_red_led[] = { { PTC1 }, { PTC0 }, { PTC7 }, { PTC3 }, { PTC4 }, { PTC5 }, { PTC2 }, { PTC8 } , {PTA1} , {PTA2} };
+class button_control {
+private:
+	DigitalIn m_butt;
+	bool previousState = 0;
+	bool isActive = 0; //changes to opposite every release
 
+public:
+
+	button_control(PinName button) :
+			m_butt(button) {
+	}
+	;
+	bool getIsActive() {
+		return this->isActive;
+	}
+	;
+	void updateState() {
+		if (!this->m_butt && this->previousState) { // if pressed
+			this->previousState = 0;
+		}
+		if (this->m_butt && !this->previousState) // if released
+		{
+			this->previousState = 1;
+			this->isActive = !this->isActive;
+		}
+	}
+	;
+
+};
+
+ledcontrols g_red_led[] = { { PTC1 }, { PTC0 }, { PTC7 }, { PTC3 }, { PTC4 }, {
+		PTC5 }, { PTC2 }, { PTC8 } };
+
+button_control butt[4] = { PTC9, PTC10, PTC11, PTC12 };
 
 int main() {
-    while (1) {}
+	while (1) {
+		butt[1].updateState();
+		if (butt[1].getIsActive()) {
+			for (int i = 0; i < 8; i++) {
+				g_red_led[i].set_brightness(0.05);
+			}
+
+		} else {
+			for (int i = 0; i < 8; i++) {
+				g_red_led[i].set_brightness(1);
+			}
+		}
+	}
 }
