03f94c35ad26c59189a5e65bbf80ead43b73408e
deenyse <47937619+deenyse@users.noreply.github.com>
2024-05-02 10:03:07 +0200

    Add files via upload

---
+++ i2c/Debug/mbed-os/symbols.d
@@ -0,0 +1,3 @@
+mbed-os/symbols.o mbed-os/symbols.d: ../mbed-os/symbols.c \
+ /home/fei/yat0008/Desktop/controller/i2c/mbed_config.h
+/home/fei/yat0008/Desktop/controller/i2c/mbed_config.h:
--- i2c/main-i2c.cpp
+++ i2c/main-i2c.cpp
@@ -1,126 +1,235 @@
-// **************************************************************************
-//
-//               Demo program for APPS labs
-//
-// Subject:      Computer Architectures and Parallel systems
-// Author:       Petr Olivka, petr.olivka@vsb.cz, 02/2022
-// Organization: Department of Computer Science, FEECS,
-//               VSB-Technical University of Ostrava, CZ
-//
-// File:         Main program for I2C bus
-//
-// **************************************************************************
-
 #include <mbed.h>
 
 #include "i2c-lib.h"
 #include "si4735-lib.h"
-
-//************************************************************************
-
 // Direction of I2C communication
 #define R	0b00000001
 #define W	0b00000000
 
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 
-DigitalOut g_led_PTA1( PTA1, 0 );
-DigitalOut g_led_PTA2( PTA2, 0 );
+DigitalOut g_led_PTA1(PTA1, 0);
+DigitalOut g_led_PTA2(PTA2, 0);
 
-DigitalIn g_but_PTC9( PTC9 );
-DigitalIn g_but_PTC10( PTC10 );
-DigitalIn g_but_PTC11( PTC11 );
-DigitalIn g_but_PTC12( PTC12 );
+DigitalIn butt[4] = { PTC9, PTC10, PTC11, PTC12 };
 
-int main( void )
-{
-	uint8_t l_S1, l_S2, l_RSSI, l_SNR, l_MULT, l_CAP;
-	uint8_t l_ack = 0;
-
-	printf( "K64F-KIT ready...\r\n" );
-
-	i2c_init();
-
-	// communication with 8 bit expander PCF8574
-
-	// start communication
-	i2c_start();
-
-	// PCF8574 addressing
-	// The address is composed from 3 parts!
-	//l_ack = i2c_output( HWADR_PCF8574 | A012 | W );
-
-	// Check l_ack! Return value must be 0!
-	// ....
-
-	//l_ack = i2c_output( Any_8_bit_value );
-	// selected LEDs should light
-
-	// stop communication
-	i2c_stop();
-
-	if ( ( l_ack = si4735_init() ) != 0 )
-	{
-		printf( "Initialization of SI4735 finish with error (%d)\r\n", l_ack );
-		return 0;
+class EinRadio {
+private:
+	uint8_t volume = 45;
+	const uint8_t min_volume = 0;
+	const uint8_t max_volume = 63;
+	int freq = 10480;
+public:
+	EinRadio() {
+		this->setVolume();
+		this->setFreq();
+		this->readFreq();
+		printf("... station tuned.\r\n\n");
 	}
-	else
-		printf( "SI4735 initialized.\r\n" );
+	void volumeUp() {
+		this->volume += 9;
 
-	printf( "\nTunig of radio station...\r\n" );
+		if (volume > 63)
+			volume = 0;
 
-	// Required frequency in MHz * 100
-	int l_freq = 10140; // Radiozurnal
+		this->setVolume();
+	}
+	void volumeDown() {
+		this->volume -= 9;
 
-	// Tuning of radio station
-	i2c_start();
-	l_ack |= i2c_output( SI4735_ADDRESS | W);
-	l_ack |= i2c_output( 0x20 );			// FM_TUNE_FREQ
-	l_ack |= i2c_output( 0x00 );			// ARG1
-	l_ack |= i2c_output( l_freq >> 8 );		// ARG2 - FreqHi
-	l_ack |= i2c_output( l_freq & 0xff );	// ARG3 - FreqLo
-	l_ack |= i2c_output( 0x00 );			// ARG4
-	i2c_stop();
-	// Check l_ack!
-	// if...
+		if (volume < 9)
+			volume = 63;
 
-	// Tuning process inside SI4735
-	wait_us( 100000 );
-	printf( "... station tuned.\r\n\n" );
+		this->setVolume();
+	}
+	void setVolume() {
+		//	uint8_t S1, S2, RSSI, SNR, MULT, CAP;
+		//	int freq3;
 
-	// Example of reading of tuned frequency
-	i2c_start();
-	l_ack |= i2c_output( SI4735_ADDRESS | W );
-	l_ack |= i2c_output( 0x22 );			// FM_TUNE_STATUS
-	l_ack |= i2c_output( 0x00 );			// ARG1
-	// repeated start
-	i2c_start();
-	// change direction of communication
-	l_ack |= i2c_output( SI4735_ADDRESS | R );
-	// read data
-	l_S1 = i2c_input();
-	i2c_ack();
-	l_S2 = i2c_input();
-	i2c_ack();
-	l_freq = ( uint32_t ) i2c_input() << 8;
-	i2c_ack();
-	l_freq |= i2c_input();
-	i2c_ack();
-	l_RSSI = i2c_input();
-	i2c_ack();
-	l_SNR = i2c_input();
-	i2c_ack();
-	l_MULT = i2c_input();
-	i2c_ack();
-	l_CAP = i2c_input();
-	i2c_nack();
-	i2c_stop();
+		i2c_start();
+		i2c_output( SI4735_ADDRESS | W);
+		i2c_output(0x12);
+		i2c_output(0x00);
+		i2c_output(0x40);
+		i2c_output(0x00);
+		i2c_output(0x00);
+		i2c_output(this->volume);
+		i2c_stop();
+	}
 
-	if ( l_ack != 0 )
-		printf( "Communication error!\r\n" );
-	else
-		printf( "Current tuned frequency: %d.%dMHz\r\n", l_freq / 100, l_freq % 100 );
+	void setFreq() {
+		i2c_start();
+		i2c_output( SI4735_ADDRESS | W);
+		i2c_output(0x20);			// FM_TUNE_FREQ
+		i2c_output(0x00);			// ARG1
+		i2c_output(this->freq >> 8);		// ARG2 - FreqHi
+		i2c_output(this->freq & 0xff);	// ARG3 - FreqLo
+		i2c_output(0x00);			// ARG4
+		i2c_stop();
+		printf("Current seted frequency: %d.%dMHz\r\n", this->freq / 100,
+				this->freq % 100);
+	}
 
+	void lookUp() {
+		this->freq += 10;
+		this->setFreq();
+	}
+
+	void lookDown() {
+		this->freq -= 10;
+		this->setFreq();
+	}
+
+	void seekUp() {
+		int ack = 0;
+		i2c_start();
+		ack |= i2c_output( SI4735_ADDRESS | W);
+		ack |= i2c_output(0x21);
+		ack |= i2c_output(0b00001100);
+		i2c_stop();
+		wait_us(100000);
+		this->readFreq();
+		printf("seekUp worked\n");
+
+	}
+
+	void seekDown() {
+		int ack = 0;
+		i2c_start();
+		ack |= i2c_output( SI4735_ADDRESS | W);
+		ack |= i2c_output(0x21);
+		ack |= i2c_output(0b00000100);
+		i2c_stop();
+
+		wait_us(1000000);
+		this->readFreq();
+		printf("seekDown worked\n");
+	}
+
+	int readFreq() {
+		uint8_t S1, S2, RSSI, SNR, MULT, CAP;
+		int freq3;
+		i2c_start();
+		i2c_output( SI4735_ADDRESS | W);
+		i2c_output(0x22);			// FM_TUNE_STATUS
+		i2c_output(0x00);
+		i2c_start();
+		// change direction of communication
+		i2c_output( SI4735_ADDRESS | R);
+		// read data
+		S1 = i2c_input();
+		i2c_ack();
+		S2 = i2c_input();
+		i2c_ack();
+		freq3 = (int) i2c_input() << 8;
+		i2c_ack();
+		freq3 |= i2c_input();
+		i2c_ack();
+		RSSI = i2c_input();
+		i2c_ack();
+		SNR = i2c_input();
+		i2c_ack();
+		MULT = i2c_input();
+		i2c_ack();
+		CAP = i2c_input();
+		i2c_nack();
+		i2c_stop();
+		this->freq = freq3;
+		this->setFreq();
+		printf("Current frequency: %d.%dMHz\n", this->freq / 100, this->freq % 100);
+		printf("Main data: RSSI: %d, SNR: %d, MULT: %d, CAP: %d\n", RSSI, SNR,
+				MULT, CAP);
+	}
+	;
+};
+
+class panelHandler {
+private:
+	uint8_t swAdress = 0b01000000;
+public:
+	void leds_on(int numBits) {
+		uint8_t ack = 0;
+		i2c_start();
+		ack |= i2c_output(this->swAdress);
+		ack |= i2c_output((1 << numBits) - 1);
+		i2c_stop();
+	}
+
+	void onAnimation() {
+		for (int i = 1; i <= 8; i++) {
+			leds_on(i);
+			wait_us(200000);
+		}
+		for (int i = 7; i >= 0; i--) {
+			leds_on(i);
+			wait_us(200000);
+		}
+	}
+
+};
+
+bool isReleased[4] = { 1, 1, 1, 1 };
+
+int main(void) {
+	printf("K64F-KIT ready...\r\n");
+	i2c_init();
+	if (si4735_init() != 0) {
+		printf("Initialization of SI4735 finish with error\n");
+		return 0;
+	} else
+		printf("SI4735 initialized.\r\n");
+
+	panelHandler myPanel;
+	EinRadio myRadio;
+	wait_us(100000);
+	for (int i = 0; i < 3; i++)
+		myPanel.onAnimation();
+
+	bool isReleased[4] = { 1, 1, 1, 1 };
+	uint32_t buttonTimer[4] = { 0, 0, 0, 0 };
+	bool validButtonTimer[4] = { 0, 0, 0, 0 };
+	while (1) {
+		for (int i = 0; i < 4; i++) {
+			if (!butt[i].read() && isReleased[i]) {
+				if (i == 0) {
+					myRadio.volumeDown();
+				}
+				if (i == 1) {
+					myRadio.volumeUp();
+				}
+				if (i == 2) {
+					if (validButtonTimer[2])
+						myRadio.seekDown();
+					else
+						myRadio.lookDown();
+				}
+				if (i == 3) {
+					if (validButtonTimer[3])
+						myRadio.seekUp();
+					else
+						myRadio.lookUp();
+				}
+				isReleased[i] = false;
+				if (!validButtonTimer[i]) {
+					validButtonTimer[i] = 1;
+				} else {
+					buttonTimer[i] = 0;
+					validButtonTimer[i] = 0;
+				}
+
+			} else if (butt[i].read() && !isReleased[i]) {
+				isReleased[i] = true;
+			}
+
+			if (validButtonTimer[i]) {
+				buttonTimer[i]++;
+				if (buttonTimer[i] > 16000) {
+					buttonTimer[i] = 0;
+					validButtonTimer[i] = 0;
+				}
+			}
+		}
+
+	}
 	return 0;
 }
-
